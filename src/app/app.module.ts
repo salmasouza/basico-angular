4fcd1f95ed5b82aad3b70b9de10bdd5bbc3a3ad5
Salma Ferreira <salma.souzza@gmail.com>
2023-10-09 15:47:26 -0300

    criado front angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { CursoAngularComponent } from './curso-angular/curso-angular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    CursoAngularComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,17 @@
+import { Component} from '@angular/core'
+
+
+@Component({
+
+    selector: 'hello',
+    template: `
+        <h1>Hello!</h1>
+    `,
+
+    
+})
+export class HelloComponent {
+
+    
+
+}
