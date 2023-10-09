bfd692be90a791286c5a13e58c318f5f8488a3e6
Salma Ferreira <salma.souzza@gmail.com>
2023-10-09 16:18:36 -0300

    adicionado componente

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -4,14 +4,38 @@ import { Component} from '@angular/core'
 @Component({
 
     selector: 'hello',
-    template: `
-        <h1>Hello!</h1>
-    `,
+    templateUrl: './hello.component.html'
 
     
 })
 export class HelloComponent {
 
-    
+    nome : string;
+    clientes: Cliente[];
 
+    constructor(){
+
+      
+
+        let fulano = new Cliente('Fulano',30);
+        let cicrano = new Cliente('Cicrano',30);
+        let beltrano = new Cliente('Beltrano',30);
+        this.clientes = [fulano, cicrano, beltrano];
+        
+    }
+
+}
+
+
+class Cliente {
+
+    constructor(
+
+        public nome : string,
+        public idade : number
+
+    ){}
+
+
+  
 }
